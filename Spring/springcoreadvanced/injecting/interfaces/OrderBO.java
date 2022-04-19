004d834ff42ae11a7acce05e5bfbc0950b8a6da2
Navitha Karimbil <101250829+NavithaK123@users.noreply.github.com>
2022-04-19 09:33:32 +0530

    Add files via upload

---
+++ Spring/springcoreadvanced/injecting/interfaces/OrderBO.java
@@ -0,0 +1,10 @@
+package com.ust1.spring.springcoreadvanced.injecting.interfaces;
+
+public interface OrderBO {
+	
+		
+		void placeOrder();
+
+	}
+
+
---
+++ Spring/springcoreadvanced/injecting/interfaces/OrderDAO.java
@@ -0,0 +1,5 @@
+package com.ust1.spring.springcoreadvanced.injecting.interfaces;
+
+public interface OrderDAO {
+	void createOrder();
+}
